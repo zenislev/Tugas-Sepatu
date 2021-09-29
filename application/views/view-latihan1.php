f787d835f739e803f4df4db6fc71b831184296b4
zenislev <fauzanrasyid222@gmail.com>
2021-09-29 07:31:41 +0700

    first commit

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk Kita belajar web programming..!!!<br>
+    Nilai1 = <?= $nilai1; ?>
+    Nilai2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=$nilai1 . " + " . 
+            $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
